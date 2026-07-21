c2f79e2072c643f6cb6378c7e6280b1bca2fe944
carseneptuns <alvaaev@gmail.com>
2026-07-21 15:42:06 +0700

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,16 @@
-const mysql = require("mysql2/promise"); 
+const mysql = require("mysql2/promise");
+
 const db = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "ecommerce_mini"
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: true
+  },
+  waitForConnections: true,
+  connectionLimit: 10,
 });
 
 module.exports = db;
