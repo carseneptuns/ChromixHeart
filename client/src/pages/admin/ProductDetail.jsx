0b1afbb472721080358edec4098b7837125f1061
carseneptuns <alvaaev@gmail.com>
2026-07-21 14:44:52 +0700

    Update ProductDetail.jsx

--- client/src/pages/admin/ProductDetail.jsx
+++ client/src/pages/admin/ProductDetail.jsx
@@ -42,7 +42,7 @@ function ProductDetail() {
             <div className="detail-image">
 
                 <img
-                    src={`http://localhost:5000/uploads/products/${product.gambar}`}
+                    src={`http://chromixheart-production-6072.up.railway.app/uploads/products/${product.gambar}`}
                     alt={product.nama_produk}
                 />
 
