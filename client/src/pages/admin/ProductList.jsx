dd16a0864c5eaf41111ea4a3955271d256c6ced9
carseneptuns <alvaaev@gmail.com>
2026-07-22 08:52:44 +0700

    Update ProductList.jsx

--- client/src/pages/admin/ProductList.jsx
+++ client/src/pages/admin/ProductList.jsx
@@ -127,7 +127,7 @@ function ProductList() {
                                         className="shop-product-image"
                                         src={
                                             product.gambar
-                                                ? `https://chromixheart-production-6072.up.railway.app/uploads/products/${product.gambar}`
+                                                ? `https://chromixheart-copy-production.up.railway.app/uploads/products/${product.gambar}`
                                                 : "/no-image.png"
                                         }
                                         alt={product.nama_produk}
