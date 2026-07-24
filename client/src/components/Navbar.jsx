d73d79d5cecec9ef6b77c8ebb585e70fa06775d2
carseneptuns <alvaaev@gmail.com>
2026-07-24 09:48:23 +0700

    misi

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "../styles/navbar.css";
 
 import NavbarSidebar from "./NavbarSidebar";
@@ -16,91 +16,108 @@ function Navbar() {
   const [isNavbarOpen, setIsNavbarOpen] = useState(false);
   const [isUserOpen, setIsUserOpen] = useState(false);
 
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 992);
+
+  useEffect(() => {
+
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 992);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () =>
+      window.removeEventListener("resize", handleResize);
+
+  }, []);
+
   return (
     <>
       <nav className="chromix-navbar">
 
         <div className="container navbar-wrapper">
 
-          {/* Logo */}
           <Link className="logo" to="/">
             ChromixHeart
           </Link>
 
-          {/* Menu */}
           <div className="navbar-content">
 
-            <ul className="nav-menu">
+            {!isMobile && (
 
-              <li className="nav-item">
-                <Link className="nav-link" to="/">
-                  Home
-                </Link>
-              </li>
+              <ul className="nav-menu">
 
-              <li className="nav-item">
-                <Link className="nav-link" to="/shop">
-                  Collections
-                </Link>
-              </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/">
+                    Home
+                  </Link>
+                </li>
 
-              <li className="nav-item dropdown">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/shop">
+                    Collections
+                  </Link>
+                </li>
 
-                <span
-                  className="nav-link dropdown-toggle custom-dropdown"
-                  data-bs-toggle="dropdown"
-                  role="button"
-                >
-                  Explore
-                </span>
+                <li className="nav-item dropdown">
 
-                <ul className="dropdown-menu custom-menu">
+                  <span
+                    className="nav-link dropdown-toggle custom-dropdown"
+                    data-bs-toggle="dropdown"
+                    role="button"
+                  >
+                    Explore
+                  </span>
 
-                  <li>
-                    <Link className="dropdown-item" to="/clothes">
-                      Clothes
-                    </Link>
-                  </li>
+                  <ul className="dropdown-menu custom-menu">
 
-                  <li>
-                    <Link className="dropdown-item" to="/skirt">
-                      Skirt
-                    </Link>
-                  </li>
+                    <li>
+                      <Link className="dropdown-item" to="/clothes">
+                        Clothes
+                      </Link>
+                    </li>
 
-                  <li>
-                    <Link className="dropdown-item" to="/shoes">
-                      Shoes
-                    </Link>
-                  </li>
+                    <li>
+                      <Link className="dropdown-item" to="/skirt">
+                        Skirt
+                      </Link>
+                    </li>
 
-                  <li>
-                    <Link className="dropdown-item" to="/jewellery">
-                      Jewellery
-                    </Link>
-                  </li>
+                    <li>
+                      <Link className="dropdown-item" to="/shoes">
+                        Shoes
+                      </Link>
+                    </li>
 
-                  <li>
-                    <Link className="dropdown-item" to="/accessories">
-                      Accessories
-                    </Link>
-                  </li>
+                    <li>
+                      <Link className="dropdown-item" to="/jewellery">
+                        Jewellery
+                      </Link>
+                    </li>
 
-                </ul>
+                    <li>
+                      <Link className="dropdown-item" to="/accessories">
+                        Accessories
+                      </Link>
+                    </li>
 
-              </li>
+                  </ul>
 
-              <li className="nav-item">
-                <Link className="nav-link" to="/about">
-                  About Us
-                </Link>
-              </li>
+                </li>
 
-            </ul>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/about">
+                    About Us
+                  </Link>
+                </li>
 
-            {/* Icons */}
+              </ul>
+
+            )}
+
             <div className="nav-icons">
 
+              {/* User Sidebar */}
               <button
                 className="icon-btn"
                 onClick={() => setIsUserOpen(true)}
@@ -108,10 +125,12 @@ function Navbar() {
                 <FiUser />
               </button>
 
+              {/* Cart */}
               <button className="icon-btn">
                 <FiShoppingCart />
               </button>
 
+              {/* Hamburger */}
               <button
                 className="menu-btn"
                 onClick={() => setIsNavbarOpen(true)}
@@ -130,6 +149,7 @@ function Navbar() {
       <NavbarSidebar
         isOpen={isNavbarOpen}
         onClose={() => setIsNavbarOpen(false)}
+        isMobile={isMobile}
       />
 
       <UserSidebar
@@ -138,167 +158,6 @@ function Navbar() {
       />
     </>
   );
-}
-
-export default Navbar;import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
-import "../styles/navbar.css";
-
-import NavbarSidebar from "./NavbarSidebar";
-import UserSidebar from "./UserSidebar";
-
-import {
-    FiShoppingCart,
-    FiMenu,
-    FiUser,
-} from "react-icons/fi";
-
-function Navbar() {
-
-    const [isNavbarOpen, setIsNavbarOpen] = useState(false);
-    const [isUserOpen, setIsUserOpen] = useState(false);
-
-    const [isMobile, setIsMobile] = useState(window.innerWidth < 992);
-
-    useEffect(() => {
-
-        const handleResize = () => {
-            setIsMobile(window.innerWidth < 992);
-        };
-
-        window.addEventListener("resize", handleResize);
-
-        return () =>
-            window.removeEventListener("resize", handleResize);
-
-    }, []);
-
-    return (
-        <>
-
-            <nav className="chromix-navbar">
-
-                <div className="container navbar-wrapper">
-
-                    <Link className="logo" to="/">
-                        ChromixHeart
-                    </Link>
-
-                    <div className="navbar-content">
-
-                        {!isMobile && (
-
-                            <ul className="nav-menu">
-
-                                <li>
-                                    <Link className="nav-link" to="/">
-                                        Home
-                                    </Link>
-                                </li>
-
-                                <li>
-                                    <Link className="nav-link" to="/shop">
-                                        Collections
-                                    </Link>
-                                </li>
-
-                                <li className="dropdown">
-
-                                    <span
-                                        className="nav-link dropdown-toggle custom-dropdown"
-                                        data-bs-toggle="dropdown"
-                                    >
-                                        Explore
-                                    </span>
-
-                                    <ul className="dropdown-menu custom-menu">
-
-                                        <li>
-                                            <Link className="dropdown-item" to="/clothes">
-                                                Clothes
-                                            </Link>
-                                        </li>
-
-                                        <li>
-                                            <Link className="dropdown-item" to="/skirt">
-                                                Skirt
-                                            </Link>
-                                        </li>
-
-                                        <li>
-                                            <Link className="dropdown-item" to="/shoes">
-                                                Shoes
-                                            </Link>
-                                        </li>
-
-                                        <li>
-                                            <Link className="dropdown-item" to="/jewellery">
-                                                Jewellery
-                                            </Link>
-                                        </li>
-
-                                        <li>
-                                            <Link className="dropdown-item" to="/accessories">
-                                                Accessories
-                                            </Link>
-                                        </li>
-
-                                    </ul>
-
-                                </li>
-
-                                <li>
-                                    <Link className="nav-link" to="/about">
-                                        About Us
-                                    </Link>
-                                </li>
-
-                            </ul>
-
-                        )}
-
-                        <div className="nav-icons">
-
-                            <button
-                                className="icon-btn"
-                                onClick={() => setIsUserOpen(true)}
-                            >
-                                <FiUser />
-                            </button>
-
-                            <button className="icon-btn">
-                                <FiShoppingCart />
-                            </button>
-
-                            <button
-                                className="menu-btn"
-                                onClick={() => setIsNavbarOpen(true)}
-                            >
-                                <FiMenu />
-                            </button>
-
-                        </div>
-
-                    </div>
-
-                </div>
-
-            </nav>
-
-            <NavbarSidebar
-                isOpen={isNavbarOpen}
-                onClose={() => setIsNavbarOpen(false)}
-                isMobile={isMobile}
-            />
-
-            <UserSidebar
-                isOpen={isUserOpen}
-                onClose={() => setIsUserOpen(false)}
-            />
-
-        </>
-    );
-
 }
 
 export default Navbar;
